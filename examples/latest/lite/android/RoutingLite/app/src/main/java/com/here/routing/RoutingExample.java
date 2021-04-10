acb57cc5f87c067a4ca713463831daf45c45502a
Zuha Khan <58361963+zuha1311@users.noreply.github.com>
2021-04-10 11:18:07 +0530

    Update RoutingExample.java

--- examples/latest/lite/android/RoutingLite/app/src/main/java/com/here/routing/RoutingExample.java
+++ examples/latest/lite/android/RoutingLite/app/src/main/java/com/here/routing/RoutingExample.java
@@ -148,7 +148,7 @@ public class RoutingExample {
         mapPolylines.add(routeMapPolyline);
 
         // Draw a circle to indicate starting point and destination.
-        addCircleMapMarker(startGeoCoordinates, R.drawable.green_dot);
+        addCircleMapMarker(startGeoCoordinates, R.drawable.green_dot); // the resources should be in png format and not vector
         addCircleMapMarker(destinationGeoCoordinates, R.drawable.green_dot);
 
         // Log maneuver instructions per route section.
